9703133d2f0b24330cb219b8fa3ef9458d5c6ec3
Stephen Long <long2know@gmail.com>
2017-11-03 11:47:13 -0400

    Toastr integration

--- aspnet-core-demo/ClientApp/app/services/notification.service.ts
+++ aspnet-core-demo/ClientApp/app/services/notification.service.ts
@@ -1,21 +1,37 @@
 import { Injectable } from '@angular/core';
-import { ToastyService, ToastyConfig, ToastyComponent, ToastOptions, ToastData } from 'ng2-toasty';
-import { Subject, Observable, Subscription } from 'rxjs/Rx';
+import * as $ from 'jquery';
+import * as toastr from 'toastr';
 
 @Injectable()
 export class NotificationService {
-    constructor(private toastyService: ToastyService) { }
+    private toastr: any;
+    private toastrOptions: any;
 
-    success(message: string) {
-        this.displayToast(message, 'success');
+    private defaultSuccess: string = "Success!";
+    private defaultError: string = "Error!";
+    private defaultInfo: string = "Info";
+
+    constructor() {
+        this.toastr = <any>toastr;
+
+        // Set defaults
+        this.toastrOptions = {
+            positionClass: 'toast-bottom-right'
+        };
+
+        this.toastr.options = this.toastrOptions;
+    }
+
+    success(message?: string) {
+        this.displayToast(message || this.defaultSuccess, 'success');
     };
 
-    error(message) {
-        this.displayToast(message, 'error');
+    error(message?: string) {
+        this.displayToast(message || this.defaultError, 'error');
     };
 
-    info(message) {
-        this.displayToast(message, 'info');
+    info(message?: string) {
+        this.displayToast(message || this.defaultInfo, 'info');
     };
 
     warning(message) {
@@ -23,44 +39,13 @@ export class NotificationService {
     };
 
     private displayToast(message: string, type: string) {
-        let interval = 1000;
-        let timeout = 5000;
-        let seconds = timeout / 1000;
-        let subscription: Subscription;
-
-        let toastOptions: ToastOptions = {
-            title: '',
-            msg: message,
-            showClose: true,
-            timeout: timeout,
-            theme: 'bootstrap',
-            onAdd: (toast: ToastData) => {
-                console.log('Toast ' + toast.id + ' has been added!');
-                // Run the timer with 1 second iterval
-                let observable = Observable.interval(interval).take(seconds);
-                // Start listen seconds beat
-                subscription = observable.subscribe((count: number) => {
-                    //// Update title of toast
-                    //toast.title = this.getTitle(seconds - count - 1);
-                    //// Update message of toast
-                    //toast.msg = this.getMessage(seconds - count - 1);
-                });
-
-            },
-            onRemove: function (toast: ToastData) {
-                console.log('Toast ' + toast.id + ' has been removed!');
-                // Stop listenning
-                subscription.unsubscribe();
-            }
-        };
-
         switch (type) {
-            case 'default': this.toastyService.default(toastOptions); break;
-            case 'info': this.toastyService.info(toastOptions); break;
-            case 'success': this.toastyService.success(toastOptions); break;
-            case 'wait': this.toastyService.wait(toastOptions); break;
-            case 'error': this.toastyService.error(toastOptions); break;
-            case 'warning': this.toastyService.warning(toastOptions); break;
+            case 'default': this.toastr.default(message); break;
+            case 'info': this.toastr.info(message); break;
+            case 'success': this.toastr.success(message); break;
+            case 'wait': this.toastr.wait(message); break;
+            case 'error': this.toastr.error(message); break;
+            case 'warning': this.toastr.warning(message); break;
         }
     }
 }
--- aspnet-core-demo/webpack.config.vendor.js
+++ aspnet-core-demo/webpack.config.vendor.js
@@ -30,6 +30,8 @@ const nonTreeShakableModules = [
     'ngx-bootstrap/popover',
     'ngx-bootstrap/dropdown',
     'ngx-bootstrap/carousel',
+    'toastr',
+    'toastr/build/toastr.css',
     'bootstrap-vertical-tabs/bootstrap.vertical-tabs.css',
     'bootstrap-toggle/css/bootstrap-toggle.css',
     'bootstrap-toggle/js/bootstrap-toggle.js',
@@ -64,7 +66,8 @@ module.exports = (env) => {
                 "window.jQuery": "jquery",
                 jQuery: "jquery",
                 Tether: 'tether',
-                Popper: 'popper.js'
+                Popper: 'popper.js',
+                toastr: 'toastr'
             }), // Maps these identifiers to the jQuery package (because Bootstrap expects it to be a global variable)
             new webpack.ContextReplacementPlugin(/\@angular\b.*\b(bundles|linker)/, path.join(__dirname, './ClientApp')), // Workaround for https://github.com/angular/angular/issues/11580
             new webpack.ContextReplacementPlugin(/angular(\\|\/)core(\\|\/)@angular/, path.join(__dirname, './ClientApp')), // Workaround for https://github.com/angular/angular/issues/14898
